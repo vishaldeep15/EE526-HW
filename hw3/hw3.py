c40c65a750265218b08a2bb725e1b0a9696e449a
Vishal Deep <visdhiman15@gmail.com>
2019-10-16 22:18:46 -0500

    Add part a of problem 1

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -14,21 +14,12 @@ import time
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-# Return a batch of training data
-def next_batch(batchSize, X, y):    
-    idx = np.arange(0 , len(X))
-    np.random.shuffle(idx)
-    idx = idx[:batchSize]
-    XBatch = X[idx]
-    yBatch = y[idx]
-
-    return XBatch, yBatch
-
-# Python optimisation variables
+# Set up learning rate, epochs, and batch size
 learning_rate = 0.5
-epochs = 100
+epochs = 10
 batch_size =500
 
+total_batch = int(len(mnist.train.labels) / batch_size)
 #Get data from tensor flow keras
 #mnist = tf.keras.datasets.mnist
 ## Split data between train and test data
@@ -46,59 +37,102 @@ batch_size =500
 #print(y)
 #print(total_batch)
 
+def layer(nInput, nOutput, X, activation):
+    W = tf.Variable(tf.random_normal([nInput, nOutput], stddev=0.01))
+    b = tf.Variable(tf.random_normal([nOutput]))
+    z = tf.add(tf.matmul(X, W), b)
+    if activation == 'ReLu':
+        nOut = tf.nn.relu(z)
+    elif activation == 'Linear':
+        nOut = z
+    else:
+        raise ValueError('activation values can only be ReLu or Linear')
+    return nOut
 
 # Placeholders for X and y
 x = tf.placeholder(tf.float32, [None, 784])
 y = tf.placeholder(tf.float32, [None, 10])
 
-# now declare the weights connecting the input to the hidden layer
-W1 = tf.Variable(tf.random_normal([784, 50], stddev=0.01), name='W1')
-b1 = tf.Variable(tf.random_normal([50]), name='b1')
-# and the weights connecting the hidden layer to the output layer
-W2 = tf.Variable(tf.random_normal([50, 50], stddev=0.01), name='W2')
-b2 = tf.Variable(tf.random_normal([50]), name='b2')
+# First Neural Network
+l1_out = layer(784, 10, x, 'Linear')
+# y_prob = tf.nn.softmax(l1_out)
+# ce = tf.reduce_mean(-tf.reduce_sum(y_prob * tf.log(y), reduction_indices=[1]))
+ce = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=l1_out, labels=y))
 
-W3 = tf.Variable(tf.random_normal([50, 10], stddev=0.01), name='W3')
-b3 = tf.Variable(tf.random_normal([10]), name='b3')
-# calculate the output of the hidden layer
-hidden_out1 = tf.add(tf.matmul(x, W1), b1)
-hidden_out1 = tf.nn.relu(hidden_out1)
-hidden_out2 = tf.add(tf.matmul(hidden_out1, W2), b2)
-hidden_out2 = tf.nn.relu(hidden_out2)
-
-
-# now calculate the hidden layer output - in this case, let's use a softmax activated
-# output layer
-y_ = tf.nn.softmax(tf.add(tf.matmul(hidden_out2, W3), b3))
-
-y_clipped = tf.clip_by_value(y_, 1e-10, 0.9999999)
-cross_entropy = -tf.reduce_mean(tf.reduce_sum(y * tf.log(y_clipped)
-                         + (1 - y) * tf.log(1 - y_clipped), axis=1))
-
-# add an optimiser
-optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)  
-
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(ce)
 # finally setup the initialisation operator
 init_op = tf.global_variables_initializer()
 
 # define an accuracy assessment operation
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(l1_out, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # start the session
 with tf.Session() as sess:
-   # initialise the variables
-   sess.run(init_op)
-   total_batch = int(len(mnist.train.labels) / batch_size)
-   for epoch in range(epochs):
+    # initialise the variables
+    sess.run(init_op)
+   
+    for epoch in range(epochs):
         avg_cost = 0
         for i in range(total_batch):
-            batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
-#            batch_x, batch_y = next_batch(batch_size, X, y)
-#            print(batch_x.shape)
-#            print(batch_y.shape)
-            _, c = sess.run([optimiser, cross_entropy], 
-                         feed_dict={x: batch_x, y: batch_y})
+            XBatch, yBatch = mnist.train.next_batch(batch_size=batch_size)
+            _, c = sess.run([optimizer, ce], feed_dict={x: XBatch, y: yBatch})
             avg_cost += c / total_batch
         print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
-   print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
+
+    
+    testAcc = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})
+    print("Test Accuracy: ", testAcc)
+
+
+# now declare the weights connecting the input to the hidden layer
+# W1 = tf.Variable(tf.random_normal([784, 50], stddev=0.01), name='W1')
+# b1 = tf.Variable(tf.random_normal([50]), name='b1')
+# # and the weights connecting the hidden layer to the output layer
+# W2 = tf.Variable(tf.random_normal([50, 50], stddev=0.01), name='W2')
+# b2 = tf.Variable(tf.random_normal([50]), name='b2')
+
+# W3 = tf.Variable(tf.random_normal([50, 10], stddev=0.01), name='W3')
+# b3 = tf.Variable(tf.random_normal([10]), name='b3')
+# # calculate the output of the hidden layer
+# hidden_out1 = tf.add(tf.matmul(x, W1), b1)
+# hidden_out1 = tf.nn.relu(hidden_out1)
+# hidden_out2 = tf.add(tf.matmul(hidden_out1, W2), b2)
+# hidden_out2 = tf.nn.relu(hidden_out2)
+
+
+# # now calculate the hidden layer output - in this case, let's use a softmax activated
+# # output layer
+# y_ = tf.nn.softmax(tf.add(tf.matmul(hidden_out2, W3), b3))
+
+# y_clipped = tf.clip_by_value(y_, 1e-10, 0.9999999)
+# cross_entropy = -tf.reduce_mean(tf.reduce_sum(y * tf.log(y_clipped)
+#                          + (1 - y) * tf.log(1 - y_clipped), axis=1))
+
+# # add an optimiser
+# optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cross_entropy)  
+
+# # finally setup the initialisation operator
+# init_op = tf.global_variables_initializer()
+
+# # define an accuracy assessment operation
+# correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# # start the session
+# with tf.Session() as sess:
+#    # initialise the variables
+#    sess.run(init_op)
+#    total_batch = int(len(mnist.train.labels) / batch_size)
+#    for epoch in range(epochs):
+#         avg_cost = 0
+#         for i in range(total_batch):
+#             batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
+# #            batch_x, batch_y = next_batch(batch_size, X, y)
+# #            print(batch_x.shape)
+# #            print(batch_y.shape)
+#             _, c = sess.run([optimiser, cross_entropy], 
+#                          feed_dict={x: batch_x, y: batch_y})
+#             avg_cost += c / total_batch
+#         print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
+#    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
