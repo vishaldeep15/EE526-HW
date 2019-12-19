3f03b038e34f5777efc44da3404f5a0385f1f87f
Vishal Deep <visdhiman15@gmail.com>
2019-12-18 20:02:03 -0600

    Update and tweaked hyperparameters

--- finalproject/acrobot-v1.py
+++ finalproject/acrobot-v1.py
@@ -12,13 +12,14 @@ class DQN:
         self.env = env
         self.state_size = env.observation_space.shape[0]
         self.action_size = env.action_space.n
-        self.memory = deque(maxlen=10000)
-        self.gamma = 0.95
-        self.epsilon = 1
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.01
+        self.memory = deque(maxlen=100000)
+        self.gamma = 0.99
+        self.epsilon = 0.5
+        self.epsilon_min = 0.001
+        self.epsilon_decay = 0.9992
+        self.learning_rate = 1e-4
         self.batch_size = 64
+        self.tau = 1e-3
         # Create model
         # Two seperate models, one for doing predictions 
         # and other for tracking target values
@@ -30,8 +31,8 @@ class DQN:
         model   = Sequential()
         state_shape  = self.env.observation_space.shape
         model.add(Dense(5, input_dim=state_shape[0], activation="relu"))
-        model.add(Dense(5, activation="relu"))
-        model.add(Dense(5, activation="relu"))
+        model.add(Dense(10, activation="relu"))
+        # model.add(Dense(5, activation="relu"))
         model.add(Dense(self.env.action_space.n))
         model.compile(loss="mean_squared_error", optimizer=Adam(lr=self.learning_rate))
         return model
@@ -58,7 +59,7 @@ class DQN:
                 Q_future = np.max(self.target_model.predict(next_state)[0])
                 target[0][action] = reward + Q_future * self.gamma
             # Fit the model
-            self.model.fit(state, target, epochs=10, verbose=0)
+            self.model.fit(state, target, epochs=1, verbose=0)
 
     # Updates the weights in target NN
     def target_train(self):
@@ -66,14 +67,17 @@ class DQN:
         weights = self.model.get_weights()
         target_weights = self.target_model.get_weights()
         for i in range(len(target_weights)):
-            target_weights[i] = weights[i]
+            target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
         self.target_model.set_weights(target_weights)
+        # Update the epsilon to lower  exploration rate
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
 
     # Chooses action
     def action(self, state):
-        # Update the epsilon to lower  exploration rate
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+        # # Update the epsilon to lower  exploration rate
+        # self.epsilon *= self.epsilon_decay
+        # self.epsilon = max(self.epsilon_min, self.epsilon)
         if np.random.rand() < self.epsilon:
             #Choose random action
             return self.env.action_space.sample()
@@ -87,42 +91,47 @@ class DQN:
     def save_weights(self, name):
         self.model.save_weights(name)
 
-def main():
+def main(render=False):
     env = gym.make('Acrobot-v1')
     # state_size = env.observation_space.shape[0]
     # # action_size = env.action_space.n
     DQNAgent = DQN(env)
     state_size = DQNAgent.state_size
     done = False
-    trials = 100
+    episodes = 500
     trial_len = 500
 
-    for trial in range(trials):
+    for episode in range(episodes):
+        rewards = []
         # reset environment
         state = env.reset()
         state = np.reshape(state, [1, state_size])
 
         for step in range(trial_len):
-            env.render()
+            if render:
+                env.render()
             # Choose action
             action = DQNAgent.action(state)
             # Get next state and reward
             next_state, reward, done, _ = env.step(action)
-            reward = reward if not done else -10
+            # reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])
             DQNAgent.save(state, action, reward, next_state, done)
 
             # Train prediction Neural Networks
             DQNAgent.train()
             # Update target NN periodically
-            if step%100 == 0:
-                print("Updating target Network")
+            if step%10 == 0:
+                # print("Updating target Network")
                 DQNAgent.target_train()
-            
+            # Append Rewards
+            rewards.append(reward)            
             # Update state
             state = next_state
             if done:
-                print(f"Trial: {trial}, Score: {trial_len-step}, e: {DQNAgent.epsilon:.4} ")
+                print("episode: {}, score: {}, epsilon: {:.6}, Rewards: {}"
+                      .format(episode, trial_len-step, DQNAgent.epsilon, np.sum(rewards)))
+                # print(f"Episode:{episode},Score:{trial_len-step}/{trial_len},Epsilon:{DQNAgent.epsilon}")
                 break
 
 if __name__ == "__main__":
